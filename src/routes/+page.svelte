cf680fd27094527942a779e1e5315a8f8f4dcb42
Yudistira Adi Wahyudi R <yudistira19@gmail.com>
2024-06-12 20:48:19 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let count = 0;
+</script>
+
+<h1>Counter: {count}</h1>
+
+<button on:click={() => count++}>Increase</button>
+<button on:click={() => count--}>Decrease</button>
